452ce99a9fc18aa2ddb93e640fc9cb0f92daf93e
suchok94 <suchok94@gmail.com>
2024-06-27 21:18:37 +0300

    add test get_name User

--- tests/test_user_get_name.py
+++ tests/test_user_get_name.py
@@ -3,7 +3,8 @@ from src.user import User
 
 
 @pytest.mark.parametrize('name, age, extend_result',
-                         [("Ivan", 16, 'Ivan')])
+                         [("Ivan", 16, 'Ivan'),
+                          ("qweasd123", 16, 'qweasd123')])
 def test_get_name(name, age, extend_result):
     user = User(name, age)
     assert user.get_name() == extend_result
