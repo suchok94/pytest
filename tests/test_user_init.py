6ee88567582c954d978f1d568dd564f4eb5fbf44
suchok94 <suchok94@gmail.com>
2024-06-27 21:25:25 +0300

    add test in test_user_init

--- tests/test_user_init.py
+++ tests/test_user_init.py
@@ -10,8 +10,11 @@ def test_init_possitive(name, age, extend_result):
 
 
 
-
-def test_init_negative():
-
-    user = User('Ivan', 'шестнадцать')
-    assert isinstance(user.name, str)  and isinstance(user.age, int)
+@pytest.mark.parametrize('name, age',
+                         [("Ivan", 'шестнадцать'),
+                          ('Ivan', 666),
+                          ('Ivan', -20)
+                          ])
+def test_init_negative(name, age):
+    user = User(name, age)
+    assert isinstance(user.name, str)  and isinstance(user.age, int) and user.age in range(1, 100)
